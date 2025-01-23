d172dbd6b9f238e82d2be6fb25114297454a273f
kauanegrao <kauarodiguesngrao@gmail.com>
2025-01-23 12:27:10 -0400

    ..

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1>Kauã rodrigues Negrão</h1>
+<p>idade:18 anos</p>
+<p>telefone de contato:(67) 99204 9293</p>
